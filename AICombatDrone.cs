3ab4bb12532755ec406e12ce1ace42aac4e9496d
Sergey Tihonov <stcoder@live.ru>
2013-04-30 18:32:27 +0600

    Обновление класса боевого дрона

--- AICombatDrone.cs
+++ AICombatDrone.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 
 /**
  * Базовый класс искуственного интеллекта боевеого дрона.
+ * 
+ * Должен уметь защищать солдат.
+ * Если дрон атакует врага C, а враг B атакует солдата А и он в радиусе области сенсора то атакавать врага B.
  */
 class AICombatDrone : MonoBehaviour
 {
+    // Базовая скорость передвижения.
+    private float basicSpeedMove = 1.0f;
+
+    // Скорость передвижения во время турбо.
+    private float turboSpeedMove = 2.5f;
+
+    // Базовая скорость разворота.
+    private float baseSpeedRotate = 7.0f;
+
     // Количество соправаждаемых целей.
     private int maxTargets = 2;
 
@@ -15,6 +27,33 @@ class AICombatDrone : MonoBehaviour
     // Радиус сенсоров для определения целей.
     private float sensorRadius = 5.0f;
 
-    // Угол обзора.
+    // Угол обзора для атаки.
     private float viewAngle = 70.0f;
+
+    // Базовая точка позиции.
+    private Vector3 basePointPosition;
+
+    // Базовая точка поворота.
+    private Quaternion basePointRotate;
+
+    // Текущая точка позиции.
+    private Vector3 currentPointPosition;
+
+    // Текущая точка поворта.
+    private Quaternion currentPointRotate;
+
+    // Список оружий типа Transform.
+    // Можно будет менять оружия.
+    public List<Transform> weapons = new List<Transform>();
+
+    private void Start()
+    {
+        basePointPosition = currentPointPosition = transform.position;
+        basePointRotate = currentPointRotate = transform.rotation;
+
+        if (weapons == null || weapons.Count <= 0)
+        {
+            // попытаться найти оружие.
+        }
+    }
 }
